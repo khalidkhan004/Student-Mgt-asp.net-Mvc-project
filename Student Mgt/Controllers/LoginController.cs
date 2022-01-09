d9d793f8025e01a1e0fd24087562c7e2d59e96c7
Afaq khalid <Afaq khalid@DESKTOP-FTJU5U6>
2022-01-09 09:08:14 +0500

    Add project files.

---
+++ Student Mgt/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Student_Mgt.Models;
+
+namespace Student_Mgt.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+          [HttpPost]
+          [ValidateAntiForgeryToken]
+        public ActionResult Index(user objck)
+        {
+            if(ModelState.IsValid)
+            {
+                using (Student_MgtEntities db = new Student_MgtEntities())
+                {
+                    var obj = db.users.Where(a => a.username.Equals(objck.username) && a.password.Equals(objck.password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.id.ToString();
+                        Session["UserName"] = obj.username.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("" ,"The UserName and Password is Incorrect!");
+                    }
+                }
+            }
+
+           
+            return View(objck);
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index" ,"Login");
+        }
+
+    }
+}
